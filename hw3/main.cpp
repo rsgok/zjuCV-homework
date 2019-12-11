c6582854e45ad7c3329eb7a214c7e21fe57c9ed9
wkk5194 <kysoowjk@163.com>
2019-12-12 04:44:02 +0800

    hw3 添加人脸识别能力

--- hw3/main.cpp
+++ hw3/main.cpp
@@ -8,9 +8,11 @@ using namespace std;
 
 Size standardSize = Size(128,128);
 int k = 40; // 默认40张人脸图
-float energyPercent = 0.4;
+float energyPercent = 0.5;
 
 int main(int argc, char** argv ) {
+    cout <<"input energyPercent:\n";
+    cin >> energyPercent;
     // compression_params
     vector<int> compression_params;
     compression_params.push_back(IMWRITE_PNG_COMPRESSION);
@@ -22,9 +24,9 @@ int main(int argc, char** argv ) {
     
     vector<Mat> faceMats; // 存放所有face
     cout<<"--------READING DATA---------"<<endl;
-    string datadir = "./JAFFE/train";
+    string trainDatadir = "./JAFFE/train";
     vector<string> imgpaths;
-    glob(datadir+"/*.tiff",imgpaths,false);
+    glob(trainDatadir+"/*.tiff",imgpaths,false);
     k=imgpaths.size();
     cout<<"img number: "<<k<<endl;
     for (int i=0; i<k; i++)
@@ -34,8 +36,6 @@ int main(int argc, char** argv ) {
         Mat img = imread(item, IMREAD_GRAYSCALE); // single channel
         // resize
         resize(img, img, standardSize);
-        // normalize
-        normalize(img, img, 255, 0, NORM_MINMAX);
         faceMats.push_back(img);
     }
     cout<<endl;
@@ -48,7 +48,9 @@ int main(int argc, char** argv ) {
     Mat allPicVector = Mat::zeros(k,height*width,CV_32FC1);
     for (int i = 0; i < k; i++)
     {
-        Mat picVector = faceMats[i].reshape(1,1);
+        Mat picVector = faceMats[i].reshape(1,1).clone();
+        // normalize
+        // normalize(picVector, picVector, 255, 0, NORM_MINMAX);
         picVector.convertTo(picVector,CV_32FC1);
         if(i==0)
         {
@@ -106,12 +108,80 @@ int main(int argc, char** argv ) {
     int eigenCount = eigenVectors.rows*energyPercent;
     cout<<"eigen count: "<<eigenCount<<endl;
 
-    // 显示10个特征脸
-    for(int i=0;i<10;i++){
+    Mat eigenM = Mat(eigenCount,height*width,CV_32FC1);
+    // 构建映射空间
+    for(int i=0; i<eigenCount; i++){
         Mat eigenFace = Mat();
-        eigenFace = eigenVectors.row(i) * allPicVector;
+        eigenFace = allPicVector.t() * eigenVectors.col(i);
         eigenFace = eigenFace.t();
+        eigenFace.copyTo(eigenM.row(i));
         eigenFace = eigenFace.reshape(1,height);
-        imwrite("EigenFaces/eigenFace"+to_string(i)+".png", eigenFace, compression_params);
+        // 保存前10个特征脸
+        if(i<10) {
+            imwrite("eigenFaces/eigenFace"+to_string(i)+".png", eigenFace, compression_params);
+        }
     }
+    cout<<"eigenM Size:"<<endl;
+    cout<<"行数: "<<eigenM.rows<<endl; // should be eigenCount
+    cout<<"列数: "<<eigenM.cols<<endl; // should be height*width
+    cout<<"eigenM matrix done\n";
+    
+    cout<<endl;
+
+    // 测试
+    cout<<"--------Test EigenFace Model---------"<<endl;
+    // 求一下特征矩阵的转置
+    Mat eigenMT = Mat(height*width,eigenCount,CV_32FC1);
+    eigenMT = eigenM.t();
+    // read test face
+    string testDatadir = "./JAFFE/test/";
+    string testfile= "";
+    cout << "input your test file:\n";
+    cin >> testfile;
+    Mat testImg = imread(testDatadir+testfile, IMREAD_GRAYSCALE); // single channel
+    resize(testImg, testImg, standardSize);
+    imshow("testFace",testImg);
+    // normalize(testImg, testImg, 255, 0, NORM_MINMAX);
+    // 构建testFace的向量testMat
+    Mat testMat = testImg.reshape(1,1);
+    testMat.convertTo(testMat,CV_32FC1);
+    cout<<"testMat Size:"<<endl;
+    cout<<"行数: "<<testMat.rows<<endl; // should be 1
+    cout<<"列数: "<<testMat.cols<<endl; // should be height*width
+    cout<<"testMat matrix done\n";
+    // 计算testMat在特征空间的向量
+    Mat testVector = testMat * eigenMT;
+    cout<<"testVector Size:"<<endl;
+    cout<<"行数: "<<testVector.rows<<endl; // should be 1
+    cout<<"列数: "<<testVector.cols<<endl; // should be eigenCount
+    cout<<"testVector matrix done\n";
+    // 遍历寻找和他相似的图片
+    Mat judgeMat = Mat(1,height*width,CV_32FC1);
+    double minDistance=1e30;
+    int minI=-1;
+    for(int i=0;i<k;i++) {
+        judgeMat = allPicVector.row(i).clone();
+        Mat judgeVector = judgeMat * eigenMT;
+        if(i==0){
+            cout<<"judgeVector Size:"<<endl;
+            cout<<"行数: "<<judgeVector.rows<<endl; // should be 1
+            cout<<"列数: "<<judgeVector.cols<<endl; // should be eigenCount
+            cout<<"judgeVector matrix done\n";
+        }
+        Mat diffVector = testVector-judgeVector;
+        double distance = norm(diffVector, NORM_L2);
+        cout << "distance of image "<<imgpaths[i]<<": "<<distance<<endl;
+        if(distance<minDistance) {
+            minDistance = distance;
+            minI = i;
+        }
+    }
+    cout << "test done!\n";
+    cout << "test pic: "<< testDatadir<<testfile << endl;
+    cout << "find pic: "<< imgpaths[minI] << endl;
+    Mat resMat = faceMats[minI].reshape(1,height);
+    resMat.convertTo(resMat,CV_8UC1);
+    imshow("resFace",resMat);
+
+    waitKey(0);
 }
